3bf49850f15fe46d7735d359837285a253c11e51
tirth765 <tirth.patel6521@gmail.com>
2025-04-30 07:47:07 +0530

    kjn

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -14,7 +14,7 @@ const listCategores = async (req, res) => {
         })
     }
 
-    return res.status(200)
+    return res.status(200) 
       .json({
         success: true,
         data: categores,
